a1207d2d58465b96d01da1c8b917178192091a99
AD <codemail.ad@gmail.com>
2024-05-14 08:30:27 +0200

    Implemented constructor

--- A1_Percolation/src/main/java/PercolationStats.java
+++ A1_Percolation/src/main/java/PercolationStats.java
@@ -3,9 +3,31 @@ import edu.princeton.cs.algs4.StdStats;
 
 
 public class PercolationStats {
+    private static final double CONSTANT_95 = 1.96;
+    private final double[] results;
+    private final int numberOfTrials;
 
     // perform independent trials on an n-by-n grid
-    public PercolationStats(int n, int trials)
+    public PercolationStats(int n, int trials) {
+        if (n < 1 || trials < 1)
+            throw new IllegalArgumentException("n and trials must be >= 1");
+
+        int numberOfSites = n*n;
+        results = new double[trials];
+        numberOfTrials = trials;
+
+        for (int i = 0; i < trials; i++) {
+            Percolation p = new Percolation(n);
+
+            while (!p.percolates()) {
+                int randomRow = StdRandom.uniformInt(1, n+1);
+                int randomCol = StdRandom.uniformInt(1, n+1);
+                p.open(randomRow, randomCol);
+            }
+
+            results[i] = (double) p.numberOfOpenSites() / numberOfSites;
+        }
+    }
 
     // sample mean of percolation threshold
     public double mean()
