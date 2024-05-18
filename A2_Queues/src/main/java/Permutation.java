6e7844b1c4f432224360df772a55de68e6d776db
ad <codemail.ad@gmail.com>
2024-05-18 15:15:27 +0200

    Implement Permutation

--- A2_Queues/src/main/java/Permutation.java
+++ A2_Queues/src/main/java/Permutation.java
@@ -1,5 +1,13 @@
 import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdRandom;
 
 public class Permutation {
-    public static void main(String[] args){}
+    public static void main(String[] args){
+        int k = Integer.parseInt(args[0]);
+        String[] data = StdIn.readAllStrings();
+        StdRandom.shuffle(data);
+
+        for (int i = 0; i < k; i++)
+            System.out.println(data[i]);
+    }
 }
