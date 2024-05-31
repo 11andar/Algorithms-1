a69af9f0ba5dd6b33fc4e7a60ce3d6307c2a049b
ad <codemail.ad@gmail.com>
2024-05-31 11:49:13 +0200

    Initial LineSegment.java

---
+++ A3_CollinearPoints/src/main/java/LineSegment.java
@@ -0,0 +1,13 @@
+public class LineSegment {
+    public LineSegment(Point p, Point q){
+        // TODO: Construct a line segment between p and q
+    }
+
+    public void draw() {
+        // TODO: Draw this line segment
+    }
+
+    public String toString() {
+        // TODO: String representation
+    }
+}
