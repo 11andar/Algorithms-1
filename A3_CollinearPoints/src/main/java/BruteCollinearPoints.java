c700d964cb463a099576b43469c4390fe50d41b2
ad <codemail.ad@gmail.com>
2024-05-31 12:31:30 +0200

    Initial BruteCollinearPoints.java

---
+++ A3_CollinearPoints/src/main/java/BruteCollinearPoints.java
@@ -0,0 +1,14 @@
+public class BruteCollinearPoints {
+    public BruteCollinearPoints(Point[] points) {
+        // TODO: Find all line segments containing 4 points
+    }
+
+    public int numberOfSegments() {
+        // TODO: Return number of line segments
+    }
+
+    public LineSegment[] segments() {
+        // TODO: Return array of line segments
+    }
+
+}
