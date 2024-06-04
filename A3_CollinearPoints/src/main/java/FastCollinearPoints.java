6e1a447b4217c368944f8d3fe507d0aa22715b01
ad <codemail.ad@gmail.com>
2024-06-04 11:27:27 +0200

    Initial FastCollinarPoints.java

---
+++ A3_CollinearPoints/src/main/java/FastCollinearPoints.java
@@ -0,0 +1,13 @@
+public class FastCollinearPoints {
+    public FastCollinearPoints(Point[] points) {
+        // TODO: Find all line segments that contain 4 or more points
+    }
+
+    public int numberOfSegments() {
+        // TODO: Return number of line segments
+    }
+
+    public LineSegment[] segments() {
+        // TODO: Return array of existing line segments
+    }
+}
