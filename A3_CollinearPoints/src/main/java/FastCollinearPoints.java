8defdcd4b2e01c24cbad70dedca0c3c3738a2fdb
ad <codemail.ad@gmail.com>
2024-06-04 15:51:28 +0200

    Create tuple for (slope, point) pair

--- A3_CollinearPoints/src/main/java/FastCollinearPoints.java
+++ A3_CollinearPoints/src/main/java/FastCollinearPoints.java
@@ -6,6 +6,16 @@ public class FastCollinearPoints {
     private final ArrayList<LineSegment> segmentsList = new ArrayList<>();
     private int numberOfSegments;
 
+    private class Tuple {
+        private final double slope;
+        private final Point point;
+
+        public Tuple(double slope, Point point) {
+            this.slope = slope;
+            this.point = point;
+        }
+    }
+
     public FastCollinearPoints(Point[] points) {
         // TODO: Find all line segments that contain 4 or more points
         if (!isValid(points))
