16101fe80bfea0b9efbfec356e2c889f0233670e
ad <codemail.ad@gmail.com>
2024-06-04 11:40:34 +0200

    Validate points array

--- A3_CollinearPoints/src/main/java/FastCollinearPoints.java
+++ A3_CollinearPoints/src/main/java/FastCollinearPoints.java
@@ -1,6 +1,18 @@
+import java.util.HashSet;
+
 public class FastCollinearPoints {
     public FastCollinearPoints(Point[] points) {
         // TODO: Find all line segments that contain 4 or more points
+
+    }
+
+    private boolean isValid(Point[] points) {
+        HashSet<Point> seen = new HashSet<>();
+
+        for (Point p : points)
+            if (!seen.add(p) || p == null)
+                return false;
+        return true;
     }
 
     public int numberOfSegments() {
