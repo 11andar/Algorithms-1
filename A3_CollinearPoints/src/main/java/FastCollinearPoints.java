4e7fb0f31162a9b735d895c27a44551113b60ae8
ad <codemail.ad@gmail.com>
2024-06-05 09:12:59 +0200

    Update isValid()

--- A3_CollinearPoints/src/main/java/FastCollinearPoints.java
+++ A3_CollinearPoints/src/main/java/FastCollinearPoints.java
@@ -20,17 +20,17 @@ public class FastCollinearPoints {
         // TODO: Find all line segments that contain 4 or more points
         if (!isValid(points))
             throw new IllegalArgumentException("points array is not valid");
+
     }
 
     private boolean isValid(Point[] points) {
         if (points == null)
             return false;
 
-        HashSet<Point> seen = new HashSet<>();
-
-        for (Point p : points)
-            if (!seen.add(p) || p == null)
-                return false;
+        for (int i = 0; i < points.length-1; i++)
+            for (int j = i+1; j < points.length; j++)
+                if (points[i] == null || points[j] == null || points[i] == points[j])
+                    return false;
         return true;
     }
 
